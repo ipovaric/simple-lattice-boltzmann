c0de5bf53ed6c603ced8ef306f6bd56fb6bb8532
Igor <ipovaric@gmail.com>
2026-08-28 11:30:13 -0600

    feat: init cond, start obstacle

--- Solver.py
+++ Solver.py
@@ -9,8 +9,16 @@ def main():
 
     # lattice speeds and weights
     NL = 9
-    cxs = np.array[[0, 0, 1, 1,  1,  0, -1, -1, -1]]
-    cys = np.array[[0, 1, 1, 0, -1, -1, -1,  0,  1]]
+    cxs = np.array([0, 0, 1, 1,  1,  0, -1, -1, -1])
+    cys = np.array([0, 1, 1, 0, -1, -1, -1,  0,  1])
+    weights = np.array([4/9, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36])
+
+    # initial conditions
+    F = np.ones((Ny, Nx, NL)) + 0.01 * np.random.randn(Ny, Nx, NL)
+    F[:, :, 3] = 2.3    # init flow rate
+
+    # obstacle
+    cylinder = np.full((Ny, Nx), False)     # init a mask for entire domain
 
 if __name__ == "__main__":
     main()
